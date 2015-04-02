6d13dd7004fbe093637a8f86e41526c7869cd39f
Austin Teer <austinteer@gmail.com>
2015-04-01 19:09:54 -0700

    Add dist folder for file download

---
+++ dist/jquery.anchormenu.js
@@ -0,0 +1,35 @@
+/*! anchormenu - v1.0.0 - 2015-04-01
+* Copyright (c) 2015 Austin Teer; Licensed MIT */
+(function ($) {
+  $.anchorMenu = function (options) {
+    options = $.extend({}, $.anchorMenu.options, options);
+    
+    var htmlMenu = document.createElement('ul');
+
+    htmlMenu.setAttribute('class', options.menuClass);
+    
+    $('#' + options.parentId + ' .' + options.childrenClass).each(function(){
+      var dataMenu = $(this).attr('id');
+      var dataFull = $(this).attr('data-anchor-text');
+      var htmlMenuItem = document.createElement('li');
+      var htmlMenuLink = document.createElement('a');
+
+      htmlMenuItem.setAttribute('class', options.menuItemClass);
+      htmlMenuLink.setAttribute('href', '#' + dataMenu);
+      htmlMenuLink.innerText = dataFull;
+      htmlMenuItem.appendChild(htmlMenuLink);
+      htmlMenu.appendChild(htmlMenuItem);
+    });
+
+    return $('h1:first-child').after(htmlMenu);
+  };
+
+  // Default options.
+  $.anchorMenu.options = {
+    parentId: 'anchorLinks',
+    childrenClass: 'anchorItems',
+    menuClass: 'anchor-menu',
+    menuItemClass: 'anchor-menu__item'
+  };
+
+}(jQuery));
